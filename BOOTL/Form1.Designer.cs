89d3a1ab74c6ac29acdfd8f858bf30a5b0f83177
makarpi <karpiczenko.mateusz@gmail.com>
2017-01-22 20:01:57 +0100

    Create repository

---
+++ BOOTL/Form1.Designer.cs
@@ -0,0 +1,171 @@
+﻿namespace BOOTL
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.button1 = new System.Windows.Forms.Button();
+            this.toolStrip1 = new System.Windows.Forms.ToolStrip();
+            this.toolStripProgressBar1 = new System.Windows.Forms.ToolStripProgressBar();
+            this.groupBox1 = new System.Windows.Forms.GroupBox();
+            this.button2 = new System.Windows.Forms.Button();
+            this.memoBox = new System.Windows.Forms.TextBox();
+            this.openFileDialog1 = new System.Windows.Forms.OpenFileDialog();
+            this.button3 = new System.Windows.Forms.Button();
+            this.listBox1 = new System.Windows.Forms.ListBox();
+            this.info = new System.Windows.Forms.Button();
+            this.toolStrip1.SuspendLayout();
+            this.SuspendLayout();
+            // 
+            // button1
+            // 
+            this.button1.Location = new System.Drawing.Point(165, 41);
+            this.button1.Name = "button1";
+            this.button1.Size = new System.Drawing.Size(121, 23);
+            this.button1.TabIndex = 0;
+            this.button1.Text = "Załaduj plik *.hex";
+            this.button1.UseVisualStyleBackColor = true;
+            this.button1.Click += new System.EventHandler(this.button1_Click);
+            // 
+            // toolStrip1
+            // 
+            this.toolStrip1.Dock = System.Windows.Forms.DockStyle.Bottom;
+            this.toolStrip1.Items.AddRange(new System.Windows.Forms.ToolStripItem[] {
+            this.toolStripProgressBar1});
+            this.toolStrip1.Location = new System.Drawing.Point(0, 397);
+            this.toolStrip1.Name = "toolStrip1";
+            this.toolStrip1.Size = new System.Drawing.Size(535, 25);
+            this.toolStrip1.TabIndex = 3;
+            this.toolStrip1.Text = "toolStrip1";
+            // 
+            // toolStripProgressBar1
+            // 
+            this.toolStripProgressBar1.Name = "toolStripProgressBar1";
+            this.toolStripProgressBar1.Size = new System.Drawing.Size(100, 22);
+            // 
+            // groupBox1
+            // 
+            this.groupBox1.Location = new System.Drawing.Point(12, 12);
+            this.groupBox1.Name = "groupBox1";
+            this.groupBox1.Size = new System.Drawing.Size(147, 110);
+            this.groupBox1.TabIndex = 5;
+            this.groupBox1.TabStop = false;
+            this.groupBox1.Text = "Ustawienia portu COM";
+            // 
+            // button2
+            // 
+            this.button2.Location = new System.Drawing.Point(165, 12);
+            this.button2.Name = "button2";
+            this.button2.Size = new System.Drawing.Size(121, 23);
+            this.button2.TabIndex = 6;
+            this.button2.Text = "Wyszukaj FTDI";
+            this.button2.UseVisualStyleBackColor = true;
+            this.button2.Click += new System.EventHandler(this.button2_Click);
+            // 
+            // memoBox
+            // 
+            this.memoBox.Dock = System.Windows.Forms.DockStyle.Bottom;
+            this.memoBox.Location = new System.Drawing.Point(0, 138);
+            this.memoBox.Multiline = true;
+            this.memoBox.Name = "memoBox";
+            this.memoBox.ScrollBars = System.Windows.Forms.ScrollBars.Vertical;
+            this.memoBox.Size = new System.Drawing.Size(535, 259);
+            this.memoBox.TabIndex = 7;
+            // 
+            // openFileDialog1
+            // 
+            this.openFileDialog1.Filter = "Intel Hex files|*.hex";
+            // 
+            // button3
+            // 
+            this.button3.Location = new System.Drawing.Point(165, 99);
+            this.button3.Name = "button3";
+            this.button3.Size = new System.Drawing.Size(121, 23);
+            this.button3.TabIndex = 8;
+            this.button3.Text = "Wyślij";
+            this.button3.UseVisualStyleBackColor = true;
+            this.button3.Click += new System.EventHandler(this.button3_Click);
+            // 
+            // listBox1
+            // 
+            this.listBox1.FormattingEnabled = true;
+            this.listBox1.Location = new System.Drawing.Point(360, 12);
+            this.listBox1.Name = "listBox1";
+            this.listBox1.Size = new System.Drawing.Size(163, 108);
+            this.listBox1.TabIndex = 9;
+            // 
+            // info
+            // 
+            this.info.Location = new System.Drawing.Point(165, 70);
+            this.info.Name = "info";
+            this.info.Size = new System.Drawing.Size(121, 23);
+            this.info.TabIndex = 10;
+            this.info.Text = "Sprawdź połączenie";
+            this.info.UseVisualStyleBackColor = true;
+            this.info.Click += new System.EventHandler(this.button4_Click);
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(535, 422);
+            this.Controls.Add(this.info);
+            this.Controls.Add(this.listBox1);
+            this.Controls.Add(this.button3);
+            this.Controls.Add(this.memoBox);
+            this.Controls.Add(this.button2);
+            this.Controls.Add(this.groupBox1);
+            this.Controls.Add(this.button1);
+            this.Controls.Add(this.toolStrip1);
+            this.FormBorderStyle = System.Windows.Forms.FormBorderStyle.FixedSingle;
+            this.MaximizeBox = false;
+            this.Name = "Form1";
+            this.RightToLeft = System.Windows.Forms.RightToLeft.No;
+            this.StartPosition = System.Windows.Forms.FormStartPosition.CenterScreen;
+            this.Text = "AVR Bootloader";
+            this.toolStrip1.ResumeLayout(false);
+            this.toolStrip1.PerformLayout();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Button button1;
+        private System.Windows.Forms.ToolStrip toolStrip1;
+        private System.Windows.Forms.ToolStripProgressBar toolStripProgressBar1;
+        private System.Windows.Forms.GroupBox groupBox1;
+        private System.Windows.Forms.Button button2;
+        private System.Windows.Forms.TextBox memoBox;
+        private System.Windows.Forms.OpenFileDialog openFileDialog1;
+        private System.Windows.Forms.Button button3;
+        private System.Windows.Forms.ListBox listBox1;
+        private System.Windows.Forms.Button info;
+    }
+}
+
---
+++ BOOTL/Form1.cs
@@ -0,0 +1,209 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using System.IO;
+using FTD2XX_NET;
+
+namespace BOOTL
+{
+    public partial class Form1 : Form
+    {
+        FileStream plik;
+        string linia, rozszerzenie;
+        int liczba;
+        byte[] program = new byte[200000];
+        FTDI ft = new FTDI();
+        uint ilosc_bajtow, ilosc_urzadzen, bajtow_zapisanych;
+        FTDI.FT_DEVICE_INFO_NODE[] Lista_urzadzen;
+
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+
+        //ładowanie hexa
+        private void button1_Click(object sender, EventArgs e)
+        {
+            ilosc_bajtow = 0;
+
+            if (openFileDialog1.ShowDialog() == DialogResult.OK)
+            {
+                plik = new FileStream(openFileDialog1.FileName, FileMode.Open);
+                rozszerzenie = Path.GetExtension(openFileDialog1.FileName);
+
+                StreamReader odczyt = new StreamReader(plik);
+                
+                if (rozszerzenie == ".bin")
+                {
+                    //binarka = odczyt.ReadToEnd();
+                    //memoBox.AppendText(binarka.Substring(0, 16));
+                    //for(int i=0; i<binarka.Length; i++)
+                    //program = binarka.ToCharArray();
+                    //na razie nie ma obslugi plikow *.bin
+                }
+
+                else if (rozszerzenie == ".hex")
+                {    
+                    do
+                    {
+                        linia = odczyt.ReadLine();
+                        if (linia != null)
+                        {
+                            if (linia.Substring(7, 2) != "00") continue;
+
+                            liczba = Convert.ToInt16(linia.Substring(1, 2), 16);
+                            for (int i = 0; i < liczba; i++)
+                            {
+                                program[ilosc_bajtow++] = Convert.ToByte(linia.Substring(9 + i * 2, 2), 16);
+                            }
+                            
+                          }
+
+                    } while (linia != null);
+                }
+                odczyt.Close();
+                plik.Close();
+                memoBox.AppendText("Ilość bajtów: " + Convert.ToString(ilosc_bajtow) + ".\r\n");
+            }
+        }
+
+
+        //wyszukiwanie urządzeń ftdi
+        private void button2_Click(object sender, EventArgs e)
+        {
+            ft.GetNumberOfDevices(ref ilosc_urzadzen);
+            
+            Lista_urzadzen = new FTDI.FT_DEVICE_INFO_NODE[ilosc_urzadzen];
+            
+            ft.GetDeviceList(Lista_urzadzen);
+
+            listBox1.Items.Clear();
+            for (int i = 0; i < ilosc_urzadzen; i++)
+            {
+                listBox1.Items.Add(Convert.ToString(Lista_urzadzen[i].Description));
+            }
+
+            memoBox.AppendText("Wybierz urządzenie.\r\n");
+        }
+
+
+        //uruchomienie bootloadera
+        private void button3_Click(object sender, EventArgs e)
+        {
+            byte[] ramka_odbioru = new byte[1000];
+            uint odczytano_bajtow = 0;
+            uint rozmiar_strony = 0;
+            byte[] ramka_wyslania = new byte[1280];
+            uint count = 0;
+
+            ft.OpenBySerialNumber(Lista_urzadzen[listBox1.SelectedIndex].SerialNumber);
+            ft.SetBaudRate(9600);
+            ft.SetDataCharacteristics(FTDI.FT_DATA_BITS.FT_BITS_8, FTDI.FT_STOP_BITS.FT_STOP_BITS_1, FTDI.FT_PARITY.FT_PARITY_NONE);
+
+            ft.SetRTS(true);
+            
+            //System.Threading.Thread.Sleep(100);
+            
+            ft.SetRTS(false);
+            
+            do
+            {
+                ft.Read(ramka_odbioru, 1, ref odczytano_bajtow);
+            } while (ramka_odbioru[0] != '?');
+
+            ft.Write("ui", 2, ref bajtow_zapisanych);
+
+            ft.SetTimeouts(500, 0);
+            ft.Read(ramka_odbioru, 36, ref odczytano_bajtow);
+
+           
+            string[] dane = new string[5];
+            string temp;
+            temp = ASCIIEncoding.ASCII.GetString(ramka_odbioru);
+            dane = temp.Split(',');
+
+           // int rozmiar_strony = 0;
+            rozmiar_strony = Convert.ToUInt16(dane[0].Substring(3));
+
+            ft.Write("w", 1, ref bajtow_zapisanych);
+
+            count = (ilosc_bajtow / rozmiar_strony) + 1;
+
+            for (uint i = ilosc_bajtow; i < rozmiar_strony * count; i++)
+            {
+                program[i] = 0x55;
+            }
+
+            for (int i = 0; i < count; i++)
+            {
+                for (int j = 0; j < rozmiar_strony; j++)
+                    ramka_wyslania[j] = program[rozmiar_strony * i + j];
+                do
+                {
+                    ft.Read(ramka_odbioru, 1, ref odczytano_bajtow);
+                } while (ramka_odbioru[0] != '@');
+
+                ft.Write("1", 1, ref bajtow_zapisanych);
+                ft.Write(ramka_wyslania, rozmiar_strony, ref bajtow_zapisanych);
+            }
+
+            memoBox.AppendText("Wgrano poprawnie program!\r\n");
+
+            ft.Close();
+        }
+
+
+        //sprawdzanie połączenia
+        private void button4_Click(object sender, EventArgs e)
+        {
+            byte[] ramka_odbioru = new byte[100];
+            uint odczytano_bajtow = 0;
+
+            ft.OpenBySerialNumber(Lista_urzadzen[listBox1.SelectedIndex].SerialNumber);
+            ft.SetBaudRate(9600);
+            ft.SetDataCharacteristics(FTDI.FT_DATA_BITS.FT_BITS_8, FTDI.FT_STOP_BITS.FT_STOP_BITS_1, FTDI.FT_PARITY.FT_PARITY_NONE);
+
+            //System.Threading.Thread.Sleep(1000);
+            ft.SetRTS(true);
+           
+            ft.SetRTS(false);
+
+            do
+            {
+                ft.Read(ramka_odbioru, 1, ref odczytano_bajtow);
+            } while (ramka_odbioru[0] != '?');
+
+            ft.Write("ui", 2, ref odczytano_bajtow);
+
+            //ft.Read(
+            ft.SetTimeouts(500, 0);
+            ft.Read(ramka_odbioru, 36, ref odczytano_bajtow);
+
+            ft.Write("e", 1, ref odczytano_bajtow);
+
+            ft.Close();
+            string[] dane = new string[5];
+            string temp;
+            temp = ASCIIEncoding.ASCII.GetString(ramka_odbioru);
+            dane = temp.Split(',');
+           
+            UInt16 rozmiar_strony = 0;
+            rozmiar_strony = Convert.ToUInt16(dane[0].Substring(3));
+            
+            memoBox.AppendText("\r\nRozmiar strony: " + rozmiar_strony.ToString() + "\r\n");
+            //memoBox.AppendText("\r\nRozmiar strony: " + dane[0] + "\r\n");
+            memoBox.AppendText("Coś tam: " + dane[1] + "\r\n");
+            memoBox.AppendText("Procesor: " + dane[2] + "\r\n");
+            memoBox.AppendText("Częstotliwość taktowania: " + dane[3] + "\r\n");
+            memoBox.AppendText("Wersja bootloadera: " + dane[4] + "\r\n");
+        }
+
+    }
+}
